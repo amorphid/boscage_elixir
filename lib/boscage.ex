587e2429506c733bb5d982589331b155e5a1abb3
Michael Pope <mpope.cr@gmail.com>
2018-12-31 21:03:39 -0800

    Create initial commit

---
+++ lib/boscage.ex
@@ -0,0 +1,18 @@
+defmodule Boscage do
+  @moduledoc """
+  Documentation for Boscage.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Boscage.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
