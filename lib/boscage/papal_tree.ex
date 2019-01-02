6ab2fed28f0b31b6db5e08b2accd222f80d213c1
Michael Pope <mpope.cr@gmail.com>
2019-01-01 22:10:01 -0800

    Add &insert/3 for adding element to empty tree

--- lib/boscage/papal_tree.ex
+++ lib/boscage/papal_tree.ex
@@ -1,12 +1,20 @@
 defmodule Boscage.PapalTree do
   defstruct [
-    :size
+    :key,
+    :size,
+    :value
   ]
 
   def new() do
     %__MODULE__{
-      size: 0
+      key: nil,
+      size: 0,
+      value: nil,
     }
+  end
+
+  def insert(%__MODULE__{size: 0} = data, key, value) do
+    {:ok, struct!(data, [key: key, value: value, size: 1])}
   end
 
   def size(%__MODULE__{} = data) do
--- test/boscage/papal_tree_test.exs
+++ test/boscage/papal_tree_test.exs
@@ -3,8 +3,19 @@ defmodule Boscage.PapalTreeTest do
 
   @subject Boscage.PapalTree
 
+  describe "&insert/3" do
+    test "size increases by 1 for new element" do
+      empty = @subject.new()
+      0 = @subject.size(empty)
+      {:ok, not_empty} = @subject.insert(empty, :potato, "effin' tasty")
+      expected = 1
+      actual = @subject.size(not_empty)
+      assert expected == actual
+    end
+  end
+
   describe "&size/1" do
-    test "for empty tree" do
+    test "size zero" do
       assert @subject.size(@subject.new()) == 0
     end
   end
