4d9626a099dbb61dbada1e14d30587ce4dc7fd45
Michael Pope <mpope.cr@gmail.com>
2019-01-05 21:43:04 -0800

    Delete boilerplate functions & tests

--- lib/boscage.ex
+++ lib/boscage.ex
@@ -1,18 +1,2 @@
 defmodule Boscage do
-  @moduledoc """
-  Documentation for Boscage.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Boscage.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/boscage_test.exs
+++ test/boscage_test.exs
@@ -1,8 +1,3 @@
 defmodule BoscageTest do
   use ExUnit.Case
-  doctest Boscage
-
-  test "greets the world" do
-    assert Boscage.hello() == :world
-  end
 end
