eab0f27247976eb98785ab212ad2fa837d22c631
Michael Pope <mpope.cr@gmail.com>
2019-01-06 15:14:28 -0800

    Add insert for size 6 tree

--- test/boscage/papal_tree_test.exs
+++ test/boscage/papal_tree_test.exs
@@ -15,6 +15,8 @@ defmodule Boscage.PapalTreeTest do
 
   def key5(), do: 5
 
+  def key6(), do: 6
+
   def shifted_size0(), do: @subject.new()
 
   def shifted_size1() do
@@ -189,6 +191,52 @@ defmodule Boscage.PapalTreeTest do
     }
   end
 
+  def size7() do
+    %@subject{
+      key: key3(),
+      left: %@subject{
+        key: key1(),
+        left: %@subject{
+          key: key0(),
+          left: nil,
+          right: nil,
+          size: 1,
+          value: value0()
+        },
+        right: %@subject{
+          key: key2(),
+          left: nil,
+          right: nil,
+          size: 1,
+          value: value2()
+        },
+        size: 3,
+        value: value1()
+      },
+      right: %@subject{
+        key: key5(),
+        left: %@subject{
+          key: key4(),
+          left: nil,
+          right: nil,
+          size: 1,
+          value: value4()
+        },
+        right: %@subject{
+          key: key6(),
+          left: nil,
+          right: nil,
+          size: 1,
+          value: value6()
+        },
+        size: 3,
+        value: value5()
+      },
+      size: 7,
+      value: value3()
+    }
+  end
+
   def value0(), do: "zero"
 
   def value1(), do: "one"
@@ -201,6 +249,8 @@ defmodule Boscage.PapalTreeTest do
 
   def value5(), do: "five"
 
+  def value6(), do: "six"
+
   setup do
     %{
       key0: key0(),
@@ -209,6 +259,7 @@ defmodule Boscage.PapalTreeTest do
       key3: key3(),
       key4: key4(),
       key5: key5(),
+      key6: key6(),
       shifted_size0: shifted_size0(),
       shifted_size1: shifted_size1(),
       shifted_size2: shifted_size2(),
@@ -219,12 +270,14 @@ defmodule Boscage.PapalTreeTest do
       size4: size4(),
       size5: size5(),
       size6: size6(),
+      size7: size7(),
       value0: value0(),
       value1: value1(),
       value2: value2(),
       value3: value3(),
       value4: value4(),
       value5: value5(),
+      value6: value6()
     }
   end
 
@@ -262,6 +315,12 @@ defmodule Boscage.PapalTreeTest do
     test "new max value to size 5 tree", c do
       expected = c.size6
       {:ok, actual} = @subject.insert(c.size5, c.key5, c.value5)
+      assert expected == actual
+    end
+
+    test "new max value to size 6 tree", c do
+      expected = c.size7()
+      {:ok, actual} = @subject.insert(c.size6, c.key6, c.value6)
       assert expected == actual
     end
   end
