ebd4e261e79fd8ee24dc73a39f00bfb319d0228e
Michael Pope <mpope.cr@gmail.com>
2019-01-05 21:09:30 -0800

    Remove &insert/3 and &delete/2 (they'll be back!)

--- lib/boscage/papal_tree.ex
+++ lib/boscage/papal_tree.ex
@@ -17,133 +17,6 @@ defmodule Boscage.PapalTree do
     }
   end
 
-  def delete(
-        %__MODULE__{key: stored_key, right: %__MODULE__{}} = data,
-        search_key
-      )
-      when search_key > stored_key do
-    case delete(data.right, search_key) do
-      {:ok, %__MODULE__{size: size} = right} when size > 0 ->
-        size = data.size - 1
-        {:ok, struct!(data, right: right, size: size)}
-
-      {:ok, %__MODULE__{key: nil, size: 0}} ->
-        right = nil
-        size = data.size - 1
-        {:ok, struct!(data, right: right, size: size)}
-    end
-  end
-
-  def delete(%__MODULE__{left: %__MODULE__{}} = data, search_key) do
-    case delete(data.left, search_key) do
-      {:ok, %__MODULE__{size: size} = left} when size > 0 ->
-        size = data.size - 1
-        {:ok, struct!(data, left: left, size: size)}
-
-      {:ok, %__MODULE__{key: nil, size: 0}} ->
-        left = nil
-        size = data.size - 1
-        {:ok, struct!(data, left: left, size: size)}
-    end
-  end
-
-  def delete(%__MODULE__{key: key, left: nil, right: nil} = data, key) do
-    key2 = nil
-    value = nil
-    size = data.size - 1
-    {:ok, struct!(data, key: key2, value: value, size: size)}
-  end
-
-  def delete(%__MODULE__{}, search_key) do
-    {:error, "key #{inspect(search_key)} not found"}
-  end
-
-  def insert(%__MODULE__{size: 0} = data, key, value) do
-    {:ok, struct!(data, key: key, value: value, size: 1)}
-  end
-
-  def insert(%__MODULE__{key: key} = data, key, value) do
-    {:ok, struct!(data, key: key, value: value)}
-  end
-
-  def insert(
-        %__MODULE__{key: stored_key, left: nil, right: %__MODULE__{size: 1} = right, size: 2} =
-          center,
-        new_key,
-        value
-      )
-      when new_key > stored_key do
-    {:ok, left} = new() |> insert(center.key, center.value)
-    center_key = right.key
-    center_size = center.size + 1
-    center_value = right.value
-    {:ok, right2} = new() |> insert(new_key, value)
-
-    center2 =
-      struct!(center,
-        key: center_key,
-        left: left,
-        right: right2,
-        size: center_size,
-        value: center_value
-      )
-
-    {:ok, center2}
-  end
-
-  def insert(%__MODULE__{key: existing_key, right: nil} = data, new_key, value)
-      when new_key > existing_key do
-    {:ok, right} = new() |> insert(new_key, value)
-    increased_size = data.size + 1
-    {:ok, struct!(data, right: right, size: increased_size)}
-  end
-
-  def insert(
-        %__MODULE__{key: existing_key, right: right} = data,
-        new_key,
-        value
-      )
-      when new_key > existing_key do
-    {:ok, right2} = insert(right, new_key, value)
-    increased_size = data.size + 1
-    {:ok, struct!(data, right: right2, size: increased_size)}
-  end
-
-  def insert(
-        %__MODULE__{left: %__MODULE__{size: 1} = left, right: nil, size: 2} = center,
-        key,
-        value
-      ) do
-    {:ok, left2} = new() |> insert(key, value)
-    center_key = left.key
-    center_size = center.size + 1
-    center_value = left.value
-    {:ok, right} = new() |> insert(center.key, center.value)
-
-    center2 =
-      struct!(center,
-        key: center_key,
-        left: left2,
-        right: right,
-        size: center_size,
-        value: center_value
-      )
-
-    {:ok, center2}
-  end
-
-  def insert(%__MODULE__{left: nil} = data, new_key, value) do
-    {:ok, left} = new() |> insert(new_key, value)
-    increased_size = data.size + 1
-    {:ok, struct!(data, left: left, size: increased_size)}
-  end
-
-  def insert(%__MODULE__{left: left} = data, new_key, value) do
-    {:ok, left2} = insert(left, new_key, value)
-    increased_size = data.size + 1
-    {:ok, struct!(data, left: left2, size: increased_size)}
-  end
-
   def max(%__MODULE__{right: %__MODULE__{}} = data) do
     max(data.right)
   end
@@ -164,25 +37,25 @@ defmodule Boscage.PapalTree do
     {:ok, {{data.key, data.value}, new()}}
   end
 
-  def search(%__MODULE__{key: key} = data, key) do
-    data.value
-  end
-
-  def search(
-        %__MODULE__{key: stored_key, right: %__MODULE__{} = right},
-        search_key
-      )
-      when search_key > stored_key do
-    search(right, search_key)
-  end
-
-  def search(%__MODULE__{left: %__MODULE__{} = left}, search_key) do
-    search(left, search_key)
-  end
-
-  def search(%__MODULE__{} = _, search_key) do
-    {:error, "key #{inspect(search_key)} not found"}
-  end
+  # def search(%__MODULE__{key: key} = data, key) do
+  #   data.value
+  # end
+  #
+  # def search(
+  #       %__MODULE__{key: stored_key, right: %__MODULE__{} = right},
+  #       search_key
+  #     )
+  #     when search_key > stored_key do
+  #   search(right, search_key)
+  # end
+  #
+  # def search(%__MODULE__{left: %__MODULE__{} = left}, search_key) do
+  #   search(left, search_key)
+  # end
+  #
+  # def search(%__MODULE__{} = _, search_key) do
+  #   {:error, "key #{inspect(search_key)} not found"}
+  # end
 
   def size(%__MODULE__{} = data) do
     data.size
--- test/boscage/papal_tree_test.exs
+++ test/boscage/papal_tree_test.exs
@@ -3,151 +3,6 @@ defmodule Boscage.PapalTreeTest do
 
   @subject Boscage.PapalTree
 
-  describe "&delete/2" do
-    test "removes value for key" do
-      empty = @subject.new()
-      {key1, val1} = {:commander, "Data"}
-      {key2, val2} = {:doctor, "Crusher"}
-      {key3, val3} = {:captain, "Picard"}
-      {key4, val4} = {:admiral, "Janeway"}
-      {:ok, tree1} = @subject.insert(empty, key1, val1)
-      {:ok, tree2} = @subject.insert(tree1, key2, val2)
-      {:ok, tree3} = @subject.insert(tree2, key3, val3)
-      {:ok, tree4} = @subject.insert(tree3, key4, val4)
-      {:ok, ^tree3} = @subject.delete(tree4, key4)
-      {:ok, ^tree2} = @subject.delete(tree3, key3)
-      {:ok, ^tree1} = @subject.delete(tree2, key2)
-      {:ok, ^empty} = @subject.delete(tree1, key1)
-    end
-
-    test "returns error for missing key" do
-      key = :key
-      expected = {:error, "key #{inspect(key)} not found"}
-      actual = @subject.search(@subject.new(), key)
-      assert expected == actual
-    end
-  end
-
-  describe "&insert/3" do
-    test "new key increases size" do
-      empty = @subject.new()
-      0 = @subject.size(empty)
-      {:ok, not_empty} = @subject.insert(empty, :potato, "effin' tasty")
-      expected = 1
-      actual = @subject.size(not_empty)
-      assert expected == actual
-    end
-
-    test "duplicate key does not affect size" do
-      key = :potato
-      {:ok, new_tree} = @subject.insert(@subject.new(), key, "effin' tasty")
-      1 = @subject.size(new_tree)
-      {:ok, updated_tree} = @subject.insert(new_tree, key, "still effin' tasty")
-      expected = 1
-      actual = @subject.size(updated_tree)
-      assert expected == actual
-    end
-
-    test "Adding 2 new keys increases size by 2" do
-      empty = @subject.new()
-      0 = @subject.size(empty)
-      {:ok, size1} = @subject.insert(empty, :commander, "Data")
-      {:ok, size2} = @subject.insert(size1, :doctor, "Crusher")
-      expected = 2
-      actual = @subject.size(size2)
-      assert expected == actual
-    end
-
-    test "Adding 3 new keys increases size by 3" do
-      empty = @subject.new()
-      0 = @subject.size(empty)
-      {:ok, size1} = @subject.insert(empty, :commander, "Data")
-      {:ok, size2} = @subject.insert(size1, :doctor, "Crusher")
-      {:ok, size3} = @subject.insert(size2, :captain, "Picard")
-      expected = 3
-      actual = @subject.size(size3)
-      assert expected == actual
-    end
-
-    test "Adding 4 new keys increases size by 4" do
-      empty = @subject.new()
-      0 = @subject.size(empty)
-      {:ok, size1} = @subject.insert(empty, :commander, "Data")
-      {:ok, size2} = @subject.insert(size1, :doctor, "Crusher")
-      {:ok, size3} = @subject.insert(size2, :captain, "Picard")
-      {:ok, size4} = @subject.insert(size3, :admiral, "Janeway")
-      expected = 4
-      actual = @subject.size(size4)
-      assert expected == actual
-    end
-
-    test "Adding keys 0 to 2 in that order returns balanced tree" do
-      empty = @subject.new()
-      {key1, val1} = {0, "zero"}
-      {key2, val2} = {1, "one"}
-      {key3, val3} = {2, "two"}
-      {:ok, tree1} = @subject.insert(empty, key1, val1)
-      {:ok, tree2} = @subject.insert(tree1, key2, val2)
-
-      tree3 = %@subject{
-        key: 1,
-        left: %@subject{
-          key: 0,
-          left: nil,
-          right: nil,
-          size: 1,
-          value: "zero"
-        },
-        right: %@subject{
-          key: 2,
-          left: nil,
-          right: nil,
-          size: 1,
-          value: "two"
-        },
-        size: 3,
-        value: "one"
-      }
-
-      expected = {:ok, tree3}
-      actual = @subject.insert(tree2, key3, val3)
-      assert expected == actual
-    end
-
-    test "Adding keys 2 to 0 in that order returns balanced tree" do
-      empty = @subject.new()
-      {key1, val1} = {2, "two"}
-      {key2, val2} = {1, "one"}
-      {key3, val3} = {0, "zero"}
-      {:ok, tree1} = @subject.insert(empty, key1, val1)
-      {:ok, tree2} = @subject.insert(tree1, key2, val2)
-
-      tree3 = %@subject{
-        key: 1,
-        left: %@subject{
-          key: 0,
-          left: nil,
-          right: nil,
-          size: 1,
-          value: "zero"
-        },
-        right: %@subject{
-          key: 2,
-          left: nil,
-          right: nil,
-          size: 1,
-          value: "two"
-        },
-        size: 3,
-        value: "one"
-      }
-
-      expected = {:ok, tree3}
-      actual = @subject.insert(tree2, key3, val3)
-      assert expected == actual
-    end
-  end
-
   describe "&max/1" do
     test "size 1 tree" do
       key = 0
@@ -245,30 +100,30 @@ defmodule Boscage.PapalTreeTest do
     end
   end
 
-  describe "&search/2" do
-    test "returns value for key" do
-      empty = @subject.new()
-      {key1, val1} = {:commander, "Data"}
-      {key2, val2} = {:doctor, "Crusher"}
-      {key3, val3} = {:captain, "Picard"}
-      {key4, val4} = {:admiral, "Janeway"}
-      {:ok, tree1} = @subject.insert(empty, :commander, "Data")
-      {:ok, tree2} = @subject.insert(tree1, :doctor, "Crusher")
-      {:ok, tree3} = @subject.insert(tree2, :captain, "Picard")
-      {:ok, tree4} = @subject.insert(tree3, :admiral, "Janeway")
-      assert @subject.search(tree4, key1) == val1
-      assert @subject.search(tree4, key2) == val2
-      assert @subject.search(tree4, key3) == val3
-      assert @subject.search(tree4, key4) == val4
-    end
-
-    test "returns error for missing key" do
-      key = :key
-      expected = {:error, "key #{inspect(key)} not found"}
-      actual = @subject.search(@subject.new(), key)
-      assert expected == actual
-    end
-  end
+  # describe "&search/2" do
+  #   test "returns value for key" do
+  #     empty = @subject.new()
+  #     {key1, val1} = {:commander, "Data"}
+  #     {key2, val2} = {:doctor, "Crusher"}
+  #     {key3, val3} = {:captain, "Picard"}
+  #     {key4, val4} = {:admiral, "Janeway"}
+  #     {:ok, tree1} = @subject.insert(empty, :commander, "Data")
+  #     {:ok, tree2} = @subject.insert(tree1, :doctor, "Crusher")
+  #     {:ok, tree3} = @subject.insert(tree2, :captain, "Picard")
+  #     {:ok, tree4} = @subject.insert(tree3, :admiral, "Janeway")
+  #     assert @subject.search(tree4, key1) == val1
+  #     assert @subject.search(tree4, key2) == val2
+  #     assert @subject.search(tree4, key3) == val3
+  #     assert @subject.search(tree4, key4) == val4
+  #   end
+  #
+  #   test "returns error for missing key" do
+  #     key = :key
+  #     expected = {:error, "key #{inspect(key)} not found"}
+  #     actual = @subject.search(@subject.new(), key)
+  #     assert expected == actual
+  #   end
+  # end
 
   describe "&size/1" do
     test "empty" do
